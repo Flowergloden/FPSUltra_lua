ad753c423b520bea31adc9e410c464915a8c0020
YKSS\huizeyang <huizeyang@66y.com>
2024-07-08 18:58:52 +0800

    chore: add GAS support

--- Source/FPSUltra_lua/FPSUltra_lua.Build.cs
+++ Source/FPSUltra_lua/FPSUltra_lua.Build.cs
@@ -8,6 +8,10 @@ public class FPSUltra_lua : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"GameplayAbilities", "GameplayTags", "GameplayTasks", "Core", "CoreUObject", "Engine", "InputCore",
+			"EnhancedInput"
+		});
 	}
 }
